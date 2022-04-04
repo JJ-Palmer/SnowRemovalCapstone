5619e3bcb363ea4f9c46cb7fa671378ecf3a5235
almightyjayrod <almightyjayrod@gmail.com>
2022-04-04 15:30:04 -0400

    Almost finished (this should work)

---
+++ tempsensor/tempsensor.ino
@@ -0,0 +1,15 @@
+int sensorPin = 2;
+int val = 0;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+val = analogRead(sensorPin);
+Serial.println(val, DEC);
+
+
+delay(100);
+}
